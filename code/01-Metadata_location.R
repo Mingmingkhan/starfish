c925e647dadb8f477950c79e65146a4544c59e26
Mingmingkhan <mingmingkhan@gmail.com>
2023-12-06 16:16:06 +0000

    updating dx script

---
+++ code/01-Metadata_location.R
@@ -0,0 +1,142 @@
+
+# Import metadata --------------------------------------------------------
+
+# Note: this data has been downloaded from PANGAEA. We modified the data 
+# the CSV file to remove headers only 
+
+#Antarctic Peninsula
+AP_profile <- read.table("data/ps118_6-9_depth-coords.csv",
+                        sep = ",", header = TRUE)
+AP_profile$n <- row.names(AP_profile) #the image number
+AP_profile <- na.omit(AP_profile) #Remove ANY photos that don't have lat, long or depth 
+
+
+#Powell Basin 
+PB_profile <- read.table("data/ps118_69-1_depth-coords.csv",
+                         sep = ",", header = TRUE)
+PB_profile$n <- row.names(PB_profile) #the image number
+PB_profile <- na.omit(PB_profile) #Remove ANY photos that don't have lat, long or depth 
+
+
+
+# Create profile information  ---------------------------------------------
+
+# Compute distances between photos each photo in the profile 
+
+
+#loop computes distance between photo i, and the preceding photo                                 
+
+profile <- AP_profile
+
+#loop computes the distance between photo i, and the preceding photo
+profile$dist <- 0 #set all to 0
+profile$cum.dist <- 0 #set cumulative distance to 0 
+  
+for (i in 2:nrow(profile)){
+  profile$dist[i] <- 
+    geosphere::distHaversine(c(profile$Longitude[i-1],
+                               profile$Latitude[i-1]),
+                             c(profile$Longitude[i],
+                               profile$Latitude[i]))
+  profile$cum.dist <- cumsum(profile$dist)
+  
+}
+
+AP_profile <- profile #add this info to AP profile, now select photos every ~500 m 
+
+profile <- PB_profile 
+
+#Now rerun lines 32-44
+
+PB_profile <- profile
+
+#import the file names of marked photos 
+
+#Antarctic Peninsula 
+AP_transect <- list.files(path = "SVGs/AP/")
+AP_transect <- gsub('.svg','', AP_transect)
+AP_transect <- gsub(' ', '_', AP_transect)
+AP_transect <- as.matrix(AP_transect)
+colnames(AP_transect) <- "name"
+
+#Get metadata for the relevant photos only 
+AP_profile <- merge(AP_transect, AP_profile, by.x = "name", 
+                by.y = "name")
+
+#Powell Basin 
+
+PB_transect <- list.files(path = "SVGs/PB/")
+PB_transect <- gsub('.svg','', PB_transect)
+PB_transect <- gsub(' ', '_', PB_transect)
+PB_transect <- as.matrix(PB_transect)
+colnames(PB_transect) <- "name"
+
+#Get metadata for the relevant photos only 
+PB_profile <- merge(PB_transect, PB_profile, by.x = "name", 
+                    by.y = "name")
+
+save(AP_profile, AP_transect, PB_profile, PB_transect,
+     file = "data/profileMetadata.RData")
+
+
+# Create Figure 1  --------------------------------------------------------
+
+#load GEBCO 2019 data, downloaded from https://www.gebco.net/data_and_products/gridded_bathymetry_data/
+
+library(raster)
+library(ncdf4)
+
+ncdf4::nc_open("GEBCO/gebco_2023_n-57.498_s-79.4355_w-77.7656_e-41.9062.nc")
+gebco <- raster::raster("GEBCO/gebco_2023_n-57.498_s-79.4355_w-77.7656_e-41.9062.nc")
+
+
+## NOTE: this function is taken from the following tutorial: 
+## https://www.benjaminbell.co.uk/2019/08/bathymetric-maps-in-r-getting-and.html
+
+# Function to calculate colour break points
+# x = raster, b1 & b2 = number of divisions for each sequence, r1 & r2 = rounding value
+# Function to calculate colour break points
+# x = raster, b1 & b2 = number of divisions for each sequence, r1 & r2 = rounding value
+colbr <- function(x, b1=50, b2=50, r1=-2, r2=-2) {
+  # Min/max values of the raster (x)
+  mi <- cellStats(x, stat="min")-100
+  ma <- cellStats(x, stat="max")+100
+  # Create sequences, but only use unique numbers
+  s1 <- unique(round(seq(mi, 0, 0-mi/b1),r1))
+  s2 <- unique(round(seq(0, ma, ma/b2),r2))
+  # Combine sequence for our break points, removing duplicate 0
+  s3 <- c(s1, s2[-1])
+  # Create a list with the outputs
+  # [[1]] = length of the first sequence minus 1 (water)
+  # [[2]] = length of the second sequence minus 1 (land)
+  # [[3]] = The break points
+  x <- list(length(s1)-1, length(s2)-1, s3)
+}
+
+
+
+# Crop GEBCO to the extent of map needed 
+
+ant.e <- raster::extent(-78, -30, -75, -56)
+ant.gebco <- raster::crop(gebco, ant.e)
+ant.br <- colbr(ant.gebco)
+ant.br[[2]] <- 1 #change all terrestrial colours to white 
+
+#get country shapefiles 
+ant <- raster::getData("GADM", country = "ATA", level = 0)
+
+#colour palette
+blue.col <- colorRampPalette(c("darkblue", "lightblue"))
+
+#Plot the figure 
+windows(h = 10, w = 10)
+plot(ant.gebco, col=c(blue.col(ant.br[[1]]), terrain.colors(ant.br[[2]])), 
+     breaks=ant.br[[3]])
+plot(ant, add=TRUE)
+points(AP_profile$Longitude, AP_profile$Latitude, pch = 24, cex = 2, 
+       col = "black", bg = 'red', lwd = 2)
+points(PB_profile$Longitude, PB_profile$Latitude, pch = 24, cex = 2, 
+       col = "black", bg = 'red', lwd = 2)
+
+# This figure was further modified in Inkscape 
+
---
+++ code/02-dex.R
@@ -0,0 +1,126 @@
+# This script 
+# 1) Extracts the objects from Inkscape, where every photo is a sample,
+#     with every layer being an unique group, and every object within each layer
+#      is an individual animal. 
+# 2) Combines these with each photo to create a long dataframe 
+
+# Load Libraries ----------------------------------------------------------
+
+library(XML)
+library(dplyr)
+
+# Load the Data and Folder ------------------------------------------------
+
+load(file = "data/profileMetadata.RData")
+
+# DEX function ------------------------------------------------------------
+#returns a dataframe of every individual in the photo, along with taxonomic
+#name, object ID, and length+width of ellipse 
+
+dex <- function(filepath) {
+  #read in SVG
+  p <- xmlParse(filepath) #reads SVG
+  d <- xmlRoot(p)  
+  g <- d[3:10000] # Don't have more layers than this number
+  g <- g[-which(sapply(g, is.null))] #removes null layers, 
+  # because previous step adds room for many many taxa 
+  
+  
+  #extract all specimens (ellipses) and get specimen ids (at the moment not 
+  # particularly important for Ming's markup style)
+  f <- lapply(g, function(x) {m <- x["ellipse"]})
+  #runs through the list of layers, only keeps ellipse ID
+  
+  get_id <- function(x){
+    ids <- lapply(x[[1]], function(x){id <- xmlGetAttr(x, "id")})
+  }
+  
+  #extracts info for dex
+  
+  m <- list(seq(length(f)))
+  for(i in 1: length(f)){
+    m[[i]] <- get_id(f[i])
+  }
+  
+  ids <- unname(unlist(m)) #gets the object ID for every object
+  
+  #number of specimens for each taxa
+  n <- lapply(f, function(x){length(x)})
+  
+  
+  # Assigning Taxon Names to ObjectIDs --------------------------------------
+  
+  
+  #get taxon name from the layer names
+  taxa <- lapply(g, function(x){xmlGetAttr(x, "inkscape:label")})
+  
+  #replicate taxa for number of specimens
+  m <- list(seq(length(f)))
+  for(i in seq_len(length(g))){
+    m[[i]]<- rep(taxa[[i]], n[[i]])
+  }
+  taxa <- unlist(m)  ##THIS NUMBER GIVES YOU NUMBER OF TOTAL INDIVIDUALS IN PHOTO
+  
+  
+  # Gets Size of the Object, Can be modified to get Area --------------------
+  
+  #extract rx width of ellipse  
+  get_rx <- function(x){
+    lapply(x[[1]], function(x){xmlGetAttr(x, "rx")})
+  }
+  
+  m <- list(seq(length(f)))
+  for(i in 1: length(f)){
+    m[[i]] <- get_rx(f[i])
+  }
+  
+  rx <- as.numeric(unname(unlist(m))) ## width of each ellipse
+  
+  #extract ry - length of ellipse 
+  get_ry <- function(x){
+    lapply(x[[1]], function(x){xmlGetAttr(x, "ry")})
+  }
+  m <- list(seq(length(f)))
+  for(i in 1: length(f)){
+    m[[i]] <- get_ry(f[i])
+  }
+  
+  ry <- as.numeric(unname(unlist(m))) ##Length of each Ellipse 
+  
+  A <- pi*rx*ry*0.5*0.5 #area of each ellipse
+  
+  #combine to data frame
+  df <- data.frame(cbind(taxa, ids, rx, ry, A))
+  df$psid <- NA
+  
+  return(df) 
+}
+
+# Assign the Folder -------------------------------------------------------
+
+
+#### REMEMBER TO CHANGE THIS WD
+
+setwd("SVGs/AP")
+transect <- list.files()
+temp <- list(length(transect))
+
+for (i in 1:length(transect)){
+  file <- transect[i]
+  temp[[i]] <- dex(file)
+  temp[[i]][6] <- file
+}
+
+df <- do.call(rbind, temp)
+
+setwd(system("git rev-parse --show-toplevel", intern=T))
+
+write.csv(df, row.names = FALSE, file = "data/AP_dexOutput.csv") 
+
+
+# TAKE THIS DEX OUTPUT, and CLEAN IN EXCEL with FIND AND REPLACE 
+
+
+df <- read.csv(file = "data/PS118/PS118_69-1/2023-08-09_dexOutput.csv")
+tax.list <- unique(df$taxa)
+
